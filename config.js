0633ed1819229d314f380aa43387f51f63bc6d66
shijiaxiong <shijiaxiong@soyoung.com>
2021-07-01 16:20:07 +0800

    change message

--- config.js
+++ config.js
@@ -30,11 +30,11 @@ var config = {
     desc: {
         turn_on: "开始",
         play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
+        bannar_coming: "气球",
+        balloons_flying: "特效 duang~",
+        cake_fadein: "上蛋糕",
+        light_candle: "点燃蜡烛",
         wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        story: "自己偷偷看",
     }
 };
